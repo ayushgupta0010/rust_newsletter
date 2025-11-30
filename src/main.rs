4813afca25876893000959ffe0ab455c9269d3f3
ayushgupta0010 <ayushgupta0010@gmail.com>
2025-11-30 02:03:22 -0500

    Refactor tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use std::net::TcpListener;
-
-use sqlx::postgres::PgPoolOptions;
-
 use zero2prod::{
     configuration::get_config,
-    email_client::EmailClient,
-    startup::run,
+    startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
 
@@ -14,29 +9,8 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    let config = get_config().expect("Failed to read configuration");
-    let conn_pool = PgPoolOptions::new()
-        .idle_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(config.db.with_db());
-
-    let sender_email = config
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
-    let timeout = config.email_client.timeout();
-    let email_client = EmailClient::new(
-        config.email_client.base_url,
-        sender_email,
-        config.email_client.auth_token,
-        timeout,
-    );
-
-    let addr = format!("{}:{}", config.application.host, config.application.port);
-    let listener = TcpListener::bind(addr)?;
-
-    println!(
-        "Server running on http://{}:{}",
-        config.application.host, config.application.port
-    );
-    run(listener, conn_pool, email_client)?.await
+    let config = get_config().expect("Failed to read config.");
+    let app = Application::build(config).await?;
+    app.run_until_stopped().await?;
+    Ok(())
 }
---
+++ tests/api/helpers.rs
@@ -0,0 +1,72 @@
+use once_cell::sync::Lazy;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
+use zero2prod::configuration::{DatabaseSettings, get_config};
+use zero2prod::startup::{Application, get_connection_pool};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
+
+pub struct TestApp {
+    pub addr: String,
+    pub db_pool: PgPool,
+}
+
+pub async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
+    let config = {
+        let mut c = get_config().expect("Failed to read configuration.");
+        // Use a different database for each test case
+        c.db.db_name = Uuid::new_v4().to_string();
+        // Use a random OS port
+        c.application.port = 0;
+        c
+    };
+
+    configure_db(&config.db).await;
+
+    let app = Application::build(config.clone())
+        .await
+        .expect("Failed to build application");
+
+    let addr = format!("http://127.0.0.1:{}", app.port());
+    let _ = tokio::spawn(app.run_until_stopped());
+
+    TestApp {
+        addr,
+        db_pool: get_connection_pool(&config.db),
+    }
+}
+
+async fn configure_db(config: &DatabaseSettings) -> PgPool {
+    let mut conn = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to db");
+
+    conn.execute(format!(r#"CREATE DATABASE "{}";"#, config.db_name).as_str())
+        .await
+        .expect("Failed to create db");
+
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
